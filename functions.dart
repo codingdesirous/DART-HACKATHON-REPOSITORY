e5da4b7f5e860484cba2dabb948b1da9228f5199
kheira <kheira.Je29@gmail.com>
2024-03-19 00:55:59 -0700

    completed control and functions in dart

--- functions.dart
+++ functions.dart
@@ -1,2 +1,14 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+void main() {
+  int resultAddition = add(5, 3);
+  print('result of addition: $resultAddition');
+  int resultMultiplication = multiply(5, 3);
+  print('result of miltiplication: $resultMultiplication');
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,9 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+void main() {
+  String name = "Kheira";
+  int age = 23;
+  String school = "PowerLearn Project Academy";
+  String hobby = "cooking";
+
+  print(
+      "My name is $name, $age years old, I attend $school and my hobby is $hobby.");
+}
